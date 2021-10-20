792167de20a23f0d79406016a81e5486aa4c2063
MasyaSm <kuvalda-max@mail.ru>
2021-10-20 17:35:18 +0300

    observer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\Product;
+use App\Observers\ProductObserver;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\Schema;
@@ -29,5 +31,7 @@ class AppServiceProvider extends ServiceProvider
         Blade::if('admin', function () {
             return Auth::check() && Auth::user()->isAdmin();
         });
+
+        Product::observe(ProductObserver::class);
     }
 }
--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -3,17 +3,29 @@
 @section('title', 'Товар')
 
 @section('content')
-    <h1>{{ $product->name }}</h1>
-    <h2>{{ $product->category->name }}</h2>
-    <p>Цена: <b>{{ $product->price }} руб.</b></p>
-    <img src="{{ Storage::url($product->image) }}">
-    <p>{{ $product->description }}</p>
-    <form action="{{ route('basket-add', $product) }}" method="POST">
-        @if($product->isAvailable())
-            <button type="submit" class="btn btn-success" role="button">Добавить в корзину</button>
-        @else
-            Не доступен
-        @endif
-        @csrf
-    </form>
+<h1>{{ $product->name }}</h1>
+<h2>{{ $product->category->name }}</h2>
+<p>Цена: <b>{{ $product->price }} руб.</b></p>
+<img src="{{ Storage::url($product->image) }}">
+<p>{{ $product->description }}</p>
+
+@if($product->isAvailable())
+<form action="{{ route('basket-add', $product) }}" method="POST">
+    <button type="submit" class="btn btn-success" role="button">Добавить в корзину</button>
+    @csrf
+</form>
+@else
+<span>Не доступен</span>
+<br>
+<span>Сообщить мне о постоуплении товара</span>
+<div class="warning"></div>
+@if ($errors->get('email'))
+{!! $errors->get('email')[0] !!}
+@endif
+<form action="{{ route('subscription', $product) }}" method="POST">
+    @csrf
+    <input type="text" name="email">
+    <button type="submit" class="btn btn-warning">Отправить</button>
+</form>
+@endif
 @endsection
