47d65879f9c7f31bf78b4fb48a1539df02af62fd
MasyaSm <kuvalda-max@mail.ru>
2021-10-28 14:42:02 +0300

    Переделал корзину (не через бд ,а в сессию)

--- resources/views/auth/orders/show.blade.php
+++ resources/views/auth/orders/show.blade.php
@@ -1,52 +1,48 @@
 @extends('auth.layouts.master')
 
-@section('title', 'Заказ' . $order -> id)
+@section('title', 'Заказ ' . $order->id)
 
 @section('content')
-
-<div class="py-4">
-    <div class="container">
-        <div class="justify-content-center">
-            <div class="panel">
-                <h1>Заказ №{{ $order -> id}}</h1>
-                <p>Заказчик: <b>{{ $order -> name}}</b></p>
-                <p>Номер телефона: {{ $order -> phone}}</p>
-                <table class="table table-striped">
-                    <thead>
+    <div class="py-4">
+        <div class="container">
+            <div class="justify-content-center">
+                <div class="panel">
+                    <h1>Заказ №{{ $order->id }}</h1>
+                    <p>Заказчик: <b>{{ $order->name }}</b></p>
+                    <p>Номер телефона: <b>{{ $order->phomne }}</b></p>
+                    <table class="table table-striped">
+                        <thead>
                         <tr>
                             <th>Название</th>
                             <th>Кол-во</th>
                             <th>Цена</th>
                             <th>Стоимость</th>
                         </tr>
-                    </thead>
-                    <tbody>
+                        </thead>
+                        <tbody>
                         @foreach ($products as $product)
-                        <tr>
-                            <td>
-                                <a href="{{ route('product', [$product -> category -> code, $product -> code]) }}">
-                                    <img src="{{ Storage::url($product -> image) }}" height="56px" alt="">
-                                    {{ $product -> name }}
-                                </a>
-                            </td>
-                            <td>
-                                <span class="badge">1</span>
-                            </td>
-                            <td>{{ $product -> price }}</td>
-                            <td>{{ $product -> getPriceForCount() }}</td>
-                        </tr>
-
+                            <tr>
+                                <td>
+                                    <a href="{{ route('product', [$product->category->code, $product->code]) }}">
+                                        <img height="56px"
+                                             src="{{ Storage::url($product->image) }}">
+                                        {{ $product->name }}
+                                    </a>
+                                </td>
+                                <td><span class="badge"> {{ $product->pivot->count }}</span></td>
+                                <td>{{ $product->pivot->price }} {{ $order->currency->symbol }}</td>
+                                <td>{{ $product->pivot->price * $product->pivot->count }} {{ $order->currency->symbol }}</td>
+                            </tr>
                         @endforeach
                         <tr>
                             <td colspan="3">Общая стоимость:</td>
-                            <td>{{ $order -> calculateFullSum() }}</td>
+                            <td>{{ $order->sum }} {{ $order->currency->symbol }}</td>
                         </tr>
-                    </tbody>
-                </table>
-                <br>
+                        </tbody>
+                    </table>
+                    <br>
+                </div>
             </div>
         </div>
     </div>
-</div>
-
 @endsection
